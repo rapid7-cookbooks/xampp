62f6899a8ff535f6daaa4d42bc0026f0d48c29fa
Erran Carey <me@errancarey.com>
2013-07-10 20:50:55 -0500

    Update xampp cookbook to version 1.2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: dvwa
+# Cookbook Name:: xampp
 # Attributes:: default
 #
 # Copyright (c) 2013, Rapid7
@@ -11,3 +11,13 @@ default[:xampp][:dir] = '/opt'
 default[:xampp][:version] = '1.8.1'
 default[:xampp][:url] = "http://www.apachefriends.org/download.php?xampp-linux-#{default[:xampp][:version]}.tar.gz"
 default[:xampp][:tarball] = "xampp-linux-#{default[:xampp][:version]}.tar.gz"
+
+# Enumerated and placed into the LocationMatch tags
+default[:xampp][:security_policies] =	[
+                                        'Order deny,allow',
+                                        'Deny from all',
+                                        'Allow from ::1 127.0.0.0/8 \
+                                        fc00::/7 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 \
+                                        fe80::/10 169.254.0.0/16',
+                                        'ErrorDocument 403 /error/XAMPP_FORBIDDEN.html.var'
+                                      ]

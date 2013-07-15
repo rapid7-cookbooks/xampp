f4489fb0c1313da85d1ee51e9009d7a5332310af
Erran Carey <me@errancarey.com>
2013-07-15 16:44:14 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       'Rapid7'
 license          'Copyright (c) 2013, Rapid7. All Rights Reserved.'
 description      'Recipes to install the XAMPP web server on a node.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.4'
+version          '1.2.5'
 
 depends          'apt'
